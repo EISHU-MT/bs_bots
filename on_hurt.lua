550d8b50e604f1f2fd5299f90b058336ec9b4887
EISHU92 <donatto555@gmail.com>
2023-11-10 17:07:46 -0500

    Fix bot team chat & add callbacks

--- on_hurt.lua
+++ on_hurt.lua
@@ -1,6 +1,7 @@
 return function(self, puncher, _, _, _, damage)
 	local puncher_team = bs.get_player_team_css(puncher)
 	
+	
 	if puncher_team ~= "" then
 		local from = bots.to_2d(self.object:get_pos())
 		local to = bots.to_2d(puncher:get_pos())
@@ -9,10 +10,12 @@ return function(self, puncher, _, _, _, damage)
 			y = to.y - from.y
 		}
 		
+		RunCallbacks(BotsCallbacks.RegisteredOnHurtBot, self, puncher, damage)
+		
 		local dir = math.atan2(offset_to.y, offset_to.x) - (math.pi/2)
 		self.object:set_yaw(dir)
-		if puncher_team == self.team then
-			bots.chat(self, "send_warning_to_teammate", Name(puncher_team))
+		if puncher_team == bots.data[self.bot_name].team then
+			bots.chat(self, "send_warning_to_teammate", Name(puncher))
 		end
 		mobkit.hurt(self, damage)
 	end
