a371a97bd4e74b146675860dc74d65b2b44ed963
EISHU92 <donatto555@gmail.com>
2023-11-18 15:00:26 -0500

    Make bot jump when walking

--- movements.lua
+++ movements.lua
@@ -2,6 +2,7 @@
 bots.path_finder = {}
 bots.path_finder_running = {}
 bots.CancelPathTo = {}
+bots.renew_bot_path = {}
 
 local random = math.random
 local abs = math.abs
@@ -26,17 +27,36 @@ function bots.is_fordwarding(self)
 	return self.object:get_velocity().x ~= 0 and self.object:get_velocity().z ~= 0 and self.object:get_velocity().y ~= 0
 end
 
+function bots.is_there_y_difference(pos1, pos2)
+	return pos1.y ~= pos2.y
+end
+
 function bots.active_path_to(self, path, speed_factor) -- To object will be always a enemy.
 	local anim = "walk"
 	if not path then core.log("error", "Attempt of crash blocked!: Tried to request movement without paths.") return end
 	local timer = #path
+	-- We should clear other jobs about movements to continue
+	if bots.path_finder_running[self.bot_name] then -- There should need a old-path clear to be the new-path
+		if mobkit.timer(self, 3) then
+			bots.renew_bot_path[self.bot_name] = path
+			core.log("action", "Attempt to do add a new func in LL queue, just replaced the path and not added function.")
+		end
+		return
+	end
 	local func = function(self)
+		if bots.renew_bot_path[self.bot_name] then
+			path = table.copy(bots.renew_bot_path[self.bot_name])
+			bots.renew_bot_path[self.bot_name] = nil
+			core.log("action", "Received new path from active_path_to.")
+		end
 		if #path <= 1 then
 			bots.path_finder_running[self.bot_name] = nil
+			mobkit.animate(self, "stand")
 			return true
 		end
 		if bots.CancelPathTo[self.bot_name] then
 			bots.CancelPathTo[self.bot_name] = nil
+			mobkit.animate(self, "stand")
 			return true
 		end
 		local speed = speed_factor or 1
@@ -51,6 +71,7 @@ function bots.active_path_to(self, path, speed_factor) -- To object will be alwa
 		local total_dist = vec_dist(pos, path[#path])
 		if total_dist <= width + 0.5 then
 			bots.path_finder_running[self.bot_name] = nil
+			mobkit.animate(self, "stand")
 			return true
 		end
 		if not self.isonground then
@@ -60,15 +81,28 @@ function bots.active_path_to(self, path, speed_factor) -- To object will be alwa
 			table.remove(path, 1)
 			timer = timer - 1
 		end
+		
+		local will_jump = false
+		if bots.is_there_y_difference(path[path_iter + 1], pos) then
+			will_jump = true
+		end
+		
 		local turn_rate = self.turn_rate or 8
 		if vector.distance(pos, tpos) < width + 2 then
 			turn_rate = turn_rate + 2
 		end
 		timer = timer - self.dtime
-		if timer <= 0 then bots.path_finder_running[self.bot_name] = nil return true end
+		if timer <= 0 then
+			bots.path_finder_running[self.bot_name] = nil
+			mobkit.animate(self, "stand")
+			return true
+		end
 		
 		mobkit.turn2yaw(self, minetest.dir_to_yaw(dir), turn_rate)
-		mobkit.go_forward_horizontal(self, self.max_speed * speed)
+		mobkit.go_forward_horizontal(self, self.max_speed * speed + 0.1)
+		if will_jump then
+			mobkit.lq_freejump(self, 1)
+		end
 		mobkit.animate(self, anim)
 		bots.path_finder_running[self.bot_name] = true
 
