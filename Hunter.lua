4a5fb2bc4b50502b2acaed14fe0083af5a653e1f
EISHU92 <donatto555@gmail.com>
2023-11-18 14:58:16 -0500

    Make Hunter function be better

--- Hunter.lua
+++ Hunter.lua
@@ -15,94 +15,73 @@ function bots.Hunt(self, enemy, prty)
 	if not self then core.log("error", "Bug: The bot that are used to hunt is not found!.") return end
 	if not prty then core.log("error", "Bug: No priority found, bots.Hunt().") return end
 	
-	bots.hunting[self.bot_name] = enemy
-
-	
-	if bots.hunting[self.bot_name] then -- Should deny the actual path and do a new path.
-		bots.CancelPathTo[self.bot_name] = true
-		--return
-	end
-	
-	local func = function(self)
-		if bots.stop_hunter[self.bot_name] then
-			bots.stop_hunter[self.bot_name] = nil
-			bots.hunting[self.bot_name] = nil
-			bots.CancelPathTo[self.bot_name] = nil
-			--bots.hunter_time[self.bot_name] = nil
-			return true
-		end
-		
-		local pos = mobkit.get_stand_pos(self) or self.object:get_pos()
-		local opos = enemy:get_pos()
-		if (not opos) then
-			bots.stop_hunter[self.bot_name] = nil
-			bots.hunting[self.bot_name] = nil
-			bots.CancelPathTo[self.bot_name] = nil
-			--bots.hunter_time[self.bot_name] = nil
-			return true
-		end
-		local dist = vector.distance(pos,opos)
-		
-		if not mobkit.is_alive(enemy) then
-			bots.stop_hunter[self.bot_name] = nil
-			bots.hunting[self.bot_name] = nil
-			bots.CancelPathTo[self.bot_name] = nil
-			return true
-		end
-		
-		if bs.get_player_team_css(enemy) == "" then
-			bots.stop_hunter[self.bot_name] = nil
-			bots.hunting[self.bot_name] = nil
-			bots.CancelPathTo[self.bot_name] = nil
-			return true
-		end
-		
-		if mobkit.is_queue_empty_low(self) and self.isonground then
-			
-			local p = bots.find_path_to(CheckPos(pos), CheckPos(opos))
-			
-			if p and CountTable(p) > 0 then
-				bots.active_path_to(self, p, 1.5)
-				return
-			end
-			
-			--if GetObjectsInBotView(self, true)[1] then
-			--	bots.stop_hunter[self.bot_name] = nil
-			--	bots.hunting[self.bot_name] = nil
-			--	bots.CancelPathTo[self.bot_name] = nil
-				--bots.hunter_time[self.bot_name] = nil
-			--	return true
-			--else
-				if dist < 4 then
-					--mobkit.hq_attack(self,prty+1,enemy)
-					-- Clear high queue & Attack!
-					mobkit.clear_queue_high(self)
+	if mobkit.is_alive(enemy) then
+		if not bots.hunting[self.bot_name] or (bots.hunting[self.bot_name] and not mobkit.is_alive(bots.hunting[self.bot_name])) then
+			if Name(enemy) ~= Name(bots.hunting[self.bot_name]) then
+				bots.hunting[self.bot_name] = enemy
+				local func = function(self)
 					
-					local from = bots.to_2d(self.object:get_pos())
-					local to = bots.to_2d(enemy:get_pos())
-					local offset_to = {
-						x = to.x - from.x,
-						y = to.y - from.y
-					}
+					if bots.stop_hunter[self.bot_name] then
+						bots.stop_hunter[self.bot_name] = nil
+						bots.hunting[self.bot_name] = nil
+						bots.CancelPathTo[self.bot_name] = nil
+						mobkit.clear_queue_high(self)
+						mobkit.clear_queue_low(self)
+						return true
+					end
 					
-					local dir = math.atan2(offset_to.y, offset_to.x) - (math.pi/2)
-					self.object:set_yaw(dir)
-					self.object:set_animation(bots.bots_animations[self.bot_name].mine, bots.bots_animations[self.bot_name].anispeed, 0)
-					enemy:punch(self.object, nil, {damage_groups = {fleshy=5}}, nil)
+					local pos = mobkit.get_stand_pos(self) or self.object:get_pos()
+					local opos = enemy:get_pos()
+					
+					if not opos then
+						bots.stop_hunter[self.bot_name] = nil
+						bots.hunting[self.bot_name] = nil
+						bots.CancelPathTo[self.bot_name] = true
+						mobkit.clear_queue_high(self)
+						mobkit.clear_queue_low(self)
+						return true
+					end
+					
+					local dist = vector.distance(pos, opos)
+					
+					local p = bots.find_path_to(CheckPos(pos), CheckPos(opos))
+					
+					if p then
+						bots.active_path_to(self, p, 1.5)
+					end
 				end
-			--end
+				mobkit.queue_high(self,func,prty)
+			end
 		end
 	end
-	mobkit.queue_high(self,func,prty)
 end
---[[
+
+--						if dist < 3 then
+--							mobkit.lq_jumpattack(self, 1, enemy)
+--						end
+
 local function globalstep(dtime)
 	bots.hunter_timer = bots.hunter_timer + dtime
 	if bots.hunter_timer >= 1 then
-		for bot_name, value in pairs(bots.hunter_time) do
-			bots.hunter_time[bot_name] = bots.hunter_time[bot_name] - 1
-			if bots.hunter_time[bot_name] <= 0 then
-				bots.hunter_time[bot_name] = nil
+		if bs_match.match_is_started then
+			for bot_name, data in pairs(bots.data) do
+				if data.state == "alive" then
+					if bots.hunting[bot_name] and mobkit.is_alive(bots.hunting[bot_name]) then
+					elseif bots.hunting[bot_name] then
+						--mobkit.clear_queue_high(data.object:get_luaentity())
+						--mobkit.clear_queue_low(data.object:get_luaentity())
+						bots.Hunt(self, bots.find_near_enemy(data.object:get_luaentity()), mobkit.get_queue_priority(self)+1)
+						return true
+					end
+					if bs.get_player_team_css(enemy) == "" then
+						bots.stop_hunter[bot_name] = nil
+						bots.hunting[bot_name] = nil
+						bots.CancelPathTo[bot_name] = true
+						--mobkit.clear_queue_high(data.object:get_luaentity())
+						--mobkit.clear_queue_low(data.object:get_luaentity())
+						return true
+					end
+				end
 			end
 		end
 		bots.hunter_timer = 0
@@ -111,7 +90,7 @@ end
 
 core.register_globalstep(globalstep)
 
---]]
+
 
 
 
