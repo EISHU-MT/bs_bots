f4ad41abfb7c6f30aaf15cb7b296a62972289e0c
EISHU92 <donatto555@gmail.com>
2023-12-25 13:48:27 -0500

    Fix Hunter()

--- Hunter.lua
+++ Hunter.lua
@@ -6,8 +6,8 @@ bots.hunter_time = {}
 bots.stop_hunter = {}
 bots.hunter_timer = 0
 function bots.Hunt(self, enemy)
-	if not enemy then core.log("error", "Bug: No enemy to hunt, but bots.Hunt() is executed.") return end
-	if not self then core.log("error", "Bug: The bot that are used to hunt is not found!.") return end
+	if not enemy then return false end
+	if not self then return false end
 	if not bots.AbortPathMovementFor[self.bot_name] then
 		if BsEntities.IsEntityAlive(enemy) then
 			if not bots.hunting[self.bot_name] or (bots.hunting[self.bot_name] and not BsEntities.IsEntityAlive(bots.hunting[self.bot_name])) then
