909e50b19a563ee29c82ac4b376544af6ea877e8
EISHU92 <donatto555@gmail.com>
2024-01-09 15:52:37 -0300

    Add force hunt and mod vel for bots

--- Hunter.lua
+++ Hunter.lua
@@ -4,14 +4,24 @@
 bots.hunting = {}
 bots.hunter_time = {}
 bots.stop_hunter = {}
+bots.hunt_vel = {}
 bots.hunter_timer = 0
-function bots.Hunt(self, enemy)
+function bots.Hunt(self, enemy, vel, force)
 	if not enemy then return false end
 	if not self then return false end
 	if not bots.AbortPathMovementFor[self.bot_name] then
 		if BsEntities.IsEntityAlive(enemy) then
 			if not bots.hunting[self.bot_name] or (bots.hunting[self.bot_name] and not BsEntities.IsEntityAlive(bots.hunting[self.bot_name])) then
 				bots.hunting[self.bot_name] = enemy
+				if vel then
+					bots.hunt_vel[self.bot_name] = vel
+				end
+				return true
+			elseif force then
+				bots.hunting[self.bot_name] = enemy
+				if vel then
+					bots.hunt_vel[self.bot_name] = vel
+				end
 				return true
 			else
 				return false
@@ -28,25 +38,30 @@ function bots.GetHuntFunction(self)
 				if bots.stop_hunter[self.bot_name] then
 					bots.stop_hunter[self.bot_name] = nil
 					bots.hunting[self.bot_name] = nil
+					bots.hunt_vel[self.bot_name] = nil
+					return
 				end
 				local pos = BsEntities.GetStandPos(self)
 				local opos = BsEntities.GetStandPos(enemy)
 				if not opos then
 					bots.stop_hunter[self.bot_name] = nil
 					bots.hunting[self.bot_name] = nil
+					bots.hunt_vel[self.bot_name] = nil
 					return
 				end
 				local dist = vector.distance(pos, opos)
 				local path = bots.find_path_to(CheckPos(pos), CheckPos(opos))
 				if path then
-					bots.assign_path_to(self, path, 1.4)
+					bots.assign_path_to(self, path, bots.hunt_vel[self.bot_name] or 1.4)
 				end
 			else
 				bots.hunting[self.bot_name] = nil
 				bots.stop_hunter[self.bot_name] = nil
+				bots.hunt_vel[self.bot_name] = nil
 			end
 		else
 			bots.hunting[self.bot_name] = nil
+			bots.hunt_vel[self.bot_name] = nil
 			bots.stop_hunter[self.bot_name] = nil
 		end
 	end
